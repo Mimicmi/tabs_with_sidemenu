af79934aa45020e1b21498c678bf472885b8e290
Simon Frezard <simon.frezard@gmail.com>
2021-01-14 13:50:27 +0100

    added ionic storage, added quick description & image on home page, starting favorites build

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PhotosService } from '../services/photos.service';
 
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
@@ -12,28 +14,39 @@ export class DetailsPage implements OnInit {
   // récupérer la route active pour récup. le parametre id
   // on recup. notre PhotosService
   constructor(
-    private activatedRoute: ActivatedRoute, 
-    private photoService: PhotosService) { }
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotosService,
+    private storage: Storage) { }
 
-    // on définit l'index du tableau à 0
-    photoIndex = 0;
+  // on définit l'index du tableau à 0
+  photoIndex = 0;
 
-    // déclaration de notre élément pour l'utiliser dans page
-    // de details
-    photoDetails = this.photoService.data[this.photoIndex];
+  testText = "Je suis un test en chaîne de caractère";
+  savedImage = [];
+
+  // déclaration de notre élément pour l'utiliser dans page
+  // de details
+  photoDetails = this.photoService.data[this.photoIndex];
+
+  displayImage() {
+    this.savedImage.push(this.photoService.data[this.PhotosService.url]);
+    console.log(this.savedImage);
+    // this.storage.set('savedImage', this.savedImage);
+  }
+
+  // async getStorage() {
+  //   const data = await this.storage.get('savedImage');
+  //   console.log('Données récupérées du Storage', data);
+  //   if (data) this.savedImage = data;
+  // }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params) => {
-
-
       // on recherche l'index du tableau de photos correspondant
       // à l'id récupéré en paramètre
       this.photoIndex = this.photoService.data.findIndex(item => item.id == params.id);
-
-      console.log(this.photoService.data[this.photoIndex]);
-
+      // console.log(this.photoService.data[this.photoIndex]);
       this.photoDetails = this.photoService.data[this.photoIndex];
-
     });
   }
 
